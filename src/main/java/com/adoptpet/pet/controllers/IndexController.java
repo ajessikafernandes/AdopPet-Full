be411f97c59347b5355386aac274fa8fdc6c79a2
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2019-11-10 21:13:20 -0300

    add controller page index html

--- src/main/java/com/adoptpet/pet/controllers/IndexController.java
+++ src/main/java/com/adoptpet/pet/controllers/IndexController.java
@@ -1,18 +1,26 @@
-package com.adoptpet.pet.controller;
+package com.adoptpet.pet.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+* Implement an application main paging index.
+*
+* @author  jessikafernandes
+* @since   10/11/2019
+*/
 
 @Controller
 public class IndexController {
 
 	/***
-	 * ESSE MÉTODO CARREGA A PÁGINA(index.html) DE LOGIN DA NOSSA APLICAÇÃO
+	 * ESSE MÉTODO CARREGA A PÁGINA(index.html) PRINCIPAL DA NOSSA APLICAÇÃO
+	 * 
 	 * @return
 	 */
 	@GetMapping("/index")
-    public String index() {
-        return "index";
-    }
+	public String index() {
+		return "index";
+	}
 
 }
