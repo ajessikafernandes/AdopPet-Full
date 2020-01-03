d171794f2758c649b5c9a1bcfbc38792bce4fc2e
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-01-03 13:38:01 -0300

    add tratament error

--- src/main/java/com/adoptpet/pet/exception/ExceptionNonexistentObject.java
+++ src/main/java/com/adoptpet/pet/exception/ExceptionNonexistentObject.java
@@ -1,5 +1,12 @@
 package com.adoptpet.pet.exception;
 
+/**
+ *Handling Errors on Nonexistent Objects
+ *
+ * @author jessikafernandes
+ * @since 02/01/2020
+ */
+
 public class ExceptionNonexistentObject extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
