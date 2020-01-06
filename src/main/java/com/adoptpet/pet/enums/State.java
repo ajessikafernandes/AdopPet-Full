3c746c701afe106a4a3772ce9ddff10849b6231e
ajessikafernandes <jessika.alves.fernandes@outlook.com>
2020-01-05 21:28:25 -0300

    Create enum by states in address

---
+++ src/main/java/com/adoptpet/pet/enums/State.java
@@ -0,0 +1,14 @@
+package com.adoptpet.pet.enums;
+
+/**
+ * Enumeration for states.
+ *
+ * @author jessikafernandes
+ * @since 05/01/2020
+ */
+
+public enum State {
+
+	AC, AL, AP, AM, BH, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO
+
+}
